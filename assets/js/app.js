b2af9f0f8de000ffa463f38be84a808e054bd4a1
Yudi Andela <yudhi.andhela@gmail.com>
2019-10-04 08:52:27 +0700

    Random daftar Menu

---
+++ assets/js/app.js
@@ -0,0 +1,80 @@
+// Passing variabel yang di butuhkan
+const get_meal_btn = document.getElementById('get_meal')
+const meal_container = document.getElementById('meal')
+const api_url = 'https://www.themealdb.com/api/json/v1/1/random.php'
+
+// Ambil data dari API dengan axios
+get_meal_btn.addEventListener('click', () => {
+    axios.get(api_url)
+        .then(res => res.data)
+        .then(res => {
+            createMeal(res.meals[0])
+        })
+        .catch(e => {
+            console.warn(e)
+        })
+});
+
+const createMeal = meal => {
+    const ingredients = [];
+
+    // Ambil data ingredients dari object. 
+    // Paling banyak 20 data
+    for (let i = 1; i <= 20; i++) {
+        if (meal[`strIngredient${i}`]) {
+            ingredients.push(
+                `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+            );
+        } else {
+            // Stop if there are no more ingredients
+            break;
+        }
+    }
+
+    const newInnerHTML = `
+        <div class="row">
+            <div class="columns five">
+                <img src="${meal.strMealThumb}" alt="Meal Image">
+                ${
+                    meal.strCategory
+                        ? `<p><strong>Category:</strong> ${meal.strCategory}</p>`
+                        : ''
+                }
+                ${meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
+                ${
+                    meal.strTags
+                        ? `<p><strong>Tags:</strong> ${meal.strTags
+                                .split(',')
+                                .join(', ')}</p>`
+                        : ''
+                }
+                <h5>Ingredients:</h5>
+                <ul>
+                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
+                </ul>
+            </div>
+            <div class="columns seven">
+                <h4>${meal.strMeal}</h4>
+                <p>${meal.strInstructions}</p>
+            </div>
+        </div>
+        ${
+            meal.strYoutube
+                ? `
+        <div class="row">
+            <h5>Video Recipe</h5>
+            <div class="videoWrapper">
+                <iframe width="420" height="315"
+                src="https://www.youtube.com/embed/${meal.strYoutube.slice(-11)}">
+                </iframe>
+            </div>
+            <h5>Source From</h5>
+            <a href="${meal.strSource}">${meal.strSource}</a>
+        </div>`
+                : ''
+        }
+    `;
+
+    meal_container.innerHTML = newInnerHTML;
+
+}
